c226167abad204e14bad586aa8b4fa0c6fafce09
Tiago Silva do Nascimento <cadnuns.dimir@gmail.com>
2026-07-10 20:25:18 -0300

    fix(backend): query categories

--- CadnunsDev.MeusGastos.Backend/Infrastructure/BillCategoryRepository.cs
+++ CadnunsDev.MeusGastos.Backend/Infrastructure/BillCategoryRepository.cs
@@ -30,7 +30,7 @@ public class BillCategoryRepository : IBillCategoryRepository
     {
         return await context.BillCategories
             .AsNoTracking()
-            .Where(c => c.UserId == userId && EF.Functions.Like(c.Description, $"%{maxResults}%"))
+            .Where(c => c.UserId == userId && EF.Functions.Like(c.Description, $"%{query}%"))
             .OrderBy(c => c.Description)
             .Take(maxResults)
             .ToListAsync();
